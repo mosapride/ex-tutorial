1474d66d62e9f13a533c23c58f9fdd97ef6bac6b
mosapride <mosapride01@gmail.com>
2019-05-17 16:57:40 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
+import { MagoComponent } from './b/mago/mago.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AComponent,
+    BComponent,
+    MagoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/b/mago/mago.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mago',
+  templateUrl: './mago.component.html',
+  styleUrls: ['./mago.component.scss']
+})
+export class MagoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
